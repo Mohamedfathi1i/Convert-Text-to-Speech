dba9b4b670698d447991d8e2849b034c3b036422
mohamed fathi <fathimohamed587@gmail.com>
2022-08-01 13:30:28 +0200

    V1

---
+++ Talkify-master/src/talkify-ajax.js
@@ -0,0 +1,15 @@
+﻿talkify = talkify || {};
+talkify.http = (function ajax() {
+    var get = function (url) {
+        var keypart = (url.indexOf('?') !== -1 ? "&key=" : "?key=") + talkify.config.remoteService.apiKey;
+
+        return fetch(window.talkify.config.remoteService.host + url + keypart)
+            .then(function (res) {
+                return res.json();
+            });
+    };
+
+    return {
+        get: get
+    };
+})();
